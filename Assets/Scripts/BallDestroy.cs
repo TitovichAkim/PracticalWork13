b90d0d80483d639889ac63150fd9c6fac99501c6
TitovichAkim <121694751+TitovichAkim@users.noreply.github.com>
2023-12-19 10:20:24 +0300

    Update BallDestroy.cs

--- Assets/Scripts/BallDestroy.cs
+++ Assets/Scripts/BallDestroy.cs
@@ -1,18 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-
 public class BallDestroy : MonoBehaviour
 {
     public ParticleSystem dethPS;
-
     public void DestroyThisBall ()
     {
         dethPS.Play();
         this.gameObject.GetComponent<MeshRenderer>().enabled = false;
         Invoke("Restart", 3f);
     }
-
     public void Restart ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);

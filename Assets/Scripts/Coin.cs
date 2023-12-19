44d96df8a5d9ff516ebeed2a9f8c14190eaf48c0
TitovichAkim <121694751+TitovichAkim@users.noreply.github.com>
2023-12-19 10:22:48 +0300

    Update Coin.cs

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,11 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Coin : MonoBehaviour
 {
     public Bonus bonusScript;
-
     public void OnTriggerEnter (Collider other)
     {
         bonusScript.Trigger(other.gameObject);

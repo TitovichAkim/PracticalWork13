e1395e5a115b64547d4111ddff8fabd27853259e
TitovichAkim <121694751+TitovichAkim@users.noreply.github.com>
2023-12-19 10:21:18 +0300

    Update Bonus.cs

--- Assets/Scripts/Bonus.cs
+++ Assets/Scripts/Bonus.cs
@@ -2,15 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
 public class Bonus : MonoBehaviour
 {
     public ParticleSystem captureEffect;
     public GameObject panelWithText;
     public GameObject coin;
     public GameObject lucky;
-
-
     public void Trigger (GameObject trigger)
     {
         if(trigger.CompareTag("ball"))
@@ -22,7 +19,6 @@ public class Bonus : MonoBehaviour
             Invoke("DestroyBall", 3f);
         }
     }
-
     public void DestroyBall ()
     {
         panelWithText.SetActive(false);
